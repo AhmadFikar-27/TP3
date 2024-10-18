bdb5a4a552d326d324c0062cb9b8a577ed94c450
AhmadFikar-27 <nisajjcell@gmail.com>
2024-10-18 08:59:38 +0700

    menambahkan file

---
+++ car.py
@@ -0,0 +1,22 @@
+class Car:
+    def __init__(self, name, manufacturer, year):
+        self.name = name
+        self.manufacturer = manufacturer
+        self.year = year
+            
+car1 = Car("M3 GTR", "Bmw" , 2001)
+car2 = Car("RX-8", "Mazda" , 2002)
+car3 = Car("le Mans Quattro", "Audi" , 2003)
+
+print(car1.name)
+print(car1.manufacturer)
+print(car1.year)
+print(car2.name)
+print(car2.manufacturer)
+print(car2.year)
+print(car2.name)
+print(car2.manufacturer)
+print(car2.year)
+print(car3.name)
+print(car3.manufacturer)
+print(car3.year)
---
+++ mobil.py
@@ -0,0 +1,5 @@
+class Car:
+    def __init__(self):
+        self.name = ""
+        self.manufacturer = ""
+        self.year = 0
